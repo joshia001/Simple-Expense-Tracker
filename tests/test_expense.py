12b22f5ac03440e4cb06b73fadb6140777f11fd6
reddya465 <ashutosh.m.reddy@gmail.com>
2025-03-30 23:03:16 +1100

    Added inital .add test setup and first case

--- tests/test_expense.py
+++ tests/test_expense.py
@@ -1,8 +1,10 @@
 # tests/test_expense.py
+import json
 
+import pytest
 from typer.testing import CliRunner
 
-from expense_tracker import __app_name__, __version__, cli
+from expense_tracker import DB_READ_ERROR, SUCCESS, __app_name__, __version__, cli, tracker
 
 runner = CliRunner()
 
@@ -13,3 +15,33 @@ def test_version():
     # Define what we count as a success
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    expense = [{"Description":"Pay for Habibti", "Amount": 300.0, "Category": "General/Unassigned", "ID": 1}] # Excluded date for later date/time module use,Added amount as double for later arithmetic, included category as string (MAY REQUIRE CHANGE OF TYPE TO ENUM)
+    db_file = tmp_path / "expense.json"
+    with db_file.open("w") as db:
+        json.dump(expense, db, indent=4)
+    return db_file
+
+test_data1 = {
+    "description" : ["MSY", "Technology"],
+    "amount": 350.0,
+    "expense": {
+        "Description": "MSY Technology.",
+        "Amount": 350.0,
+        "Category": "General/Unassigned",
+        "ID": 2,
+    },
+}
+
+test_data2 = {
+    "description" : [],
+    "amount": ,
+    "expense": {
+        "Description": ".",
+        "Amount": ,
+        "Category": "General/Unassigned",
+        "ID": 3,
+    },
+}    
